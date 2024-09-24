dba5a992d1821d28777852a88ac3a947046a1bec
Natalie Rote <rote.n@stu.janesville.k12.wi.us>
2024-09-24 09:31:48 -0500

    update 9/24

--- about me/about me/MainForm.py
+++ about me/about me/MainForm.py
@@ -11,7 +11,6 @@ class MainForm(Form):
     def InitializeComponent(self):
         self._label1 = System.Windows.Forms.Label()
         self._button1 = System.Windows.Forms.Button()
-        self._button2 = System.Windows.Forms.Button()
         self._button3 = System.Windows.Forms.Button()
         self.SuspendLayout()
         # 
@@ -37,27 +36,14 @@ class MainForm(Form):
         self._button1.UseVisualStyleBackColor = False
         self._button1.Click += self.Button1Click
         # 
-        # button2
-        # 
-        self._button2.BackColor = System.Drawing.Color.DarkRed
-        self._button2.Font = System.Drawing.Font("Modern No. 20", 14.249999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
-        self._button2.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        self._button2.Location = System.Drawing.Point(271, 12)
-        self._button2.Name = "button2"
-        self._button2.Size = System.Drawing.Size(217, 65)
-        self._button2.TabIndex = 2
-        self._button2.Text = "Clear"
-        self._button2.UseVisualStyleBackColor = False
-        self._button2.Click += self.Button2Click
-        # 
         # button3
         # 
         self._button3.BackColor = System.Drawing.Color.DarkRed
         self._button3.Font = System.Drawing.Font("Modern No. 20", 14.249999, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, 0)
         self._button3.ForeColor = System.Drawing.SystemColors.ControlLightLight
-        self._button3.Location = System.Drawing.Point(580, 53)
+        self._button3.Location = System.Drawing.Point(322, 14)
         self._button3.Name = "button3"
-        self._button3.Size = System.Drawing.Size(254, 233)
+        self._button3.Size = System.Drawing.Size(208, 63)
         self._button3.TabIndex = 3
         self._button3.Text = "Exit"
         self._button3.UseVisualStyleBackColor = False
@@ -68,7 +54,6 @@ class MainForm(Form):
         self.BackColor = System.Drawing.Color.FromArgb(64, 0, 0)
         self.ClientSize = System.Drawing.Size(1093, 325)
         self.Controls.Add(self._button3)
-        self.Controls.Add(self._button2)
         self.Controls.Add(self._button1)
         self.Controls.Add(self._label1)
         self.Name = "MainForm"
@@ -78,9 +63,6 @@ class MainForm(Form):
 
     def Button1Click(self, sender, e):
         self._label1.Text = "My name is Natalie L. Rote. I have one dog, her name is Britney. I two older brothers and one younger sister. I playted basketball when I was younger and I still play. I enjoy drawing, and watching my favorite animes."
-
-    def Button2Click(self, sender, e):
-        self._lebel1.Text = ""
 
     def Button3Click(self, sender, e):
         Application.Exit()
